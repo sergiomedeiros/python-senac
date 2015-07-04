4e8be64db3a335245d686ef980b4e2d8fa14b268
Sergio Medeiros <sergio_pergunta@hotmail.com>
2015-07-04 15:21:16 -0300

    Banco de Dados alteracoes

--- combanco1.py
+++ combanco1.py
@@ -5,12 +5,29 @@ con = MySQLdb.connect(host='localhost', user='root', passwd='sergio',db='senac')
 
 x = con.cursor()
 
+"""
 x.execute("INSERT INTO alunos (nomealuno, curso, turma, turno) VALUES ('Marcos Antonio','Python','SE','TARDE');")
 x.execute("INSERT INTO alunos (nomealuno, curso, turma, turno) VALUES ('Rogeio','Python','SE','TARDE');")
 x.execute("INSERT INTO alunos (nomealuno, curso, turma, turno) VALUES ('Marcos Antonio','Python','SERGYGY','MANHA');")
 con.commit()
 
+
+# Consultar um registro no banco
+SELECT * FROM alunos where nome_aluno = "Sergio";
+
+
+
+# Deletar um registro no banco
+DELETE FROM alunos WHERE nome_aluno = "Sergio";
+
+# Alterar dados no banco
+UPDATE alunos SET nome_aluno="Fabricio" WHERE nome_aluno = "Sergio";
+
+"""
+
 x.execute("""SELECT * FROM alunos;""")
-print x.fetchall()
+print 'Listando Alunos :'
+for linha in x.fetchall():
+    print linha[0],'\n' # valor da primeira coluna
+
 con.close()
-#teste
